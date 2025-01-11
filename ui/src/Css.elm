bab35d1a7ddd40149591b0e6bc06191435b411ae
Lucas David Traverso <lucas6246@gmail.com>
2025-01-11 04:42:29 -0300

    Fix syntax in comment

--- ui/src/Css.elm
+++ ui/src/Css.elm
@@ -1,4 +1,4 @@
-module Css exposing (barras_precio, toast)
+module Css exposing (barras_precio, toasts_container, toast)
 
 import Html
 import Html.Attributes
@@ -7,6 +7,11 @@ import Html.Attributes
 barras_precio : Html.Attribute msg
 barras_precio =
     Html.Attributes.class "barras-precio"
+
+
+toasts_container : Html.Attribute msg
+toasts_container =
+    Html.Attributes.class "toasts-container"
 
 
 toast : Html.Attribute msg
