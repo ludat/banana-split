be1a3d2a1388df705a849700a756b7129197be98
jessica.saavedra <jessica.saavedra@withstensul.com>
2025-05-23 16:18:23 -0300

    Refactor vista grupo

--- ui/src/Css.elm
+++ ui/src/Css.elm
@@ -1,4 +1,4 @@
-module Css exposing (barras_precio, toasts_container, toast)
+module Css exposing (barras_precio, arrow_container, is_size_6_5, toasts_container, toast)
 
 import Html
 import Html.Attributes
@@ -7,6 +7,16 @@ import Html.Attributes
 barras_precio : Html.Attribute msg
 barras_precio =
     Html.Attributes.class "barras-precio"
+
+
+arrow_container : Html.Attribute msg
+arrow_container =
+    Html.Attributes.class "arrow-container"
+
+
+is_size_6_5 : Html.Attribute msg
+is_size_6_5 =
+    Html.Attributes.class "is-size-6-5"
 
 
 toasts_container : Html.Attribute msg
