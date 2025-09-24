5c15f9ef11e08202a6affcac459cf91aa9fd188d
Lucas David Traverso <lucas6246@gmail.com>
2025-09-24 19:56:16 -0300

    Forgot to add file

---
+++ ui/src/Utils/Http.elm
@@ -0,0 +1,23 @@
+module Utils.Http exposing (viewHttpError)
+
+import Html exposing (..)
+import Http
+
+
+viewHttpError : Http.Error -> Html a
+viewHttpError e =
+    case e of
+        Http.BadUrl string ->
+            text <| "Bad url: '" ++ string ++ "'."
+
+        Http.Timeout ->
+            text <| "Server timed out."
+
+        Http.NetworkError ->
+            text <| "Network error."
+
+        Http.BadStatus status ->
+            text <| "Bad status: '" ++ String.fromInt status ++ "'."
+
+        Http.BadBody body ->
+            text <| "Bad status: '" ++ body ++ "'."
